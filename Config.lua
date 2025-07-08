4ce15477ff2ccc1691ea53e1a9e9d7df1a6e08b5
lmohammmd <134609696+lmohammmd@users.noreply.github.com>
2025-07-08 07:47:03 +0330

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,35 @@
+Config = {
+ 
+
+
+
+SUDO_ID = {2076851562,2076851562,2076851562}, --ایدی سودو و ربات سی ال ای و جوینر و هلپر
+Full_Sudo = {2076851562}, --ایدی مالک اصلی
+Sudoid = 2076851562, --ایدی مالک اصلی ربات 
+BotCliId = 7316861113, --ایدی ربات سی ال ای     
+IDSup = 2076851562, -- توسعه دهنده
+BotJoiner = 6376097881, --ایدی ربات
+JoinToken = '0000', --توکن ربات جوینر
+UserHelper = '@DiGj1bot', --یوزرنیم ربات هلپر
+Paybot = 'DiGj1pak',
+LinkSuppoRt = 'https://t.me/joinchat/K_Tc799PB_VjMDdk', --لینک گروه پشتیبانی شما
+Channel = 'NitroSeen5', --ایدی چنل شما
+Sudo1 = 'NitroSeen5', --ایدی سودو       
+RedisIndex = 1, -- شمار رديس را در اينجا قرار دهيد (اگر نمیدونید چی هست تغییری ایجاد نکنید) --> base:Select ...-
+TD_ID = 7316861113, --شماره ردیس 
+ApiID = '1503047',
+ApiHash = '35df6ebd9a8cfc16f7742f9b8fd5dc01',
+SessionApi = 'APIBot',
+SessionCli = 'ClIBot',
+UserBot = 'telebot',
+Leader = 2076851562,
+NameBot = '【 Winston Source 】',
+
+ 
+
+
+----------------------------------------------
+} 
+
+ 
+return Config
